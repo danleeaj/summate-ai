a0bd2178a5c0551b49a82aca7ecd485a6470cf1a
Daniel Lee <anjie.wav@gmail.com>
2025-03-05 14:38:41 -0800

    Removed server constant from config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,3 @@
-## SERVER ADDRESS ------------------------------------------------------
-# This is the URL of the LM Studio local server to which the API 
-# calls will be made.
-
-SERVER_URL = "http://127.0.0.1:1234/v1/chat/completions"
-
 ## MODEL
 # This is the model LM Studio will use to perform inference.
 # Ensure that this model is downloaded in LM Studio first before 
