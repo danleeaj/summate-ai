76ff8543e7f884e4a60489b33cca2f2a0df4f241
Daniel Lee <anjie.wav@gmail.com>
2025-03-19 18:39:12 -0700

    Wrapped sample code in main function

--- src/hello.py
+++ src/hello.py
@@ -1,22 +1,27 @@
 from backend.call_llm import Autograder
 from config import MODEL
 
-# We initialize the model
-autograder = Autograder(llm_model=MODEL)
+def main():
 
-# Then we update the rubric
-autograder.set_rubric([
-    ("States that p-value is less than 0.05", 1),
-    ("States that the null hypothesis is rejected", 1),
-])
+    # We initialize the model
+    autograder = Autograder(llm_model=MODEL)
 
-# We can optionally update the temperature
-autograder.set_temperature(0.5)
+    # Then we update the rubric
+    autograder.set_rubric([
+        ("States that p-value is less than 0.05", 1),
+        ("States that the null hypothesis is rejected", 1),
+    ])
 
-student_response = """
-A p-value of 0.03 means that the alternative hypothesis is rejected, and the null is accepted.
-"""
+    # We can optionally update the temperature
+    autograder.set_temperature(0.5)
 
-# We can now ask the autograder to grade a student response
-evaluation = autograder.evaluate(student_response)
-print(evaluation)
+    student_response = """
+    A p-value of 0.03 means that the alternative hypothesis is rejected, and the null is accepted.
+    """
+
+    # We can now ask the autograder to grade a student response
+    evaluation = autograder.evaluate(student_response)
+    print(evaluation)
+
+if __name__ == "__main__":
+    main()
